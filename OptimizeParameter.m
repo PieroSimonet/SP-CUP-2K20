1bf33efc7036dbea48aa90155a65c958fa366546
Piero Simonetto <simonetto.piero@gmail.com>
2020-02-12 11:24:00 +0100

    Riorganizzazione
    Aggiunti file per la versione in cui stiamo lavorando

---
+++ OptimizeParameter.m
@@ -0,0 +1,34 @@
+function [] = OptimizeParameter()
+%OPTIMIZEPARAMETER Summary of this function goes here
+%   Detailed explanation goes here
+    [hour,minute,second] = getDifferenceInTime;
+    if isTooLong(hour,minute,second)
+        a = 1
+    else
+        a = 2
+    end
+end
+
+function [hour,minute,second] = getDifferenceInTime()
+    persistent oldTime;
+    if isempty(oldTime)
+        oldTime = clock;
+    end
+    nowTime = clock;
+    diff    = nowTime - oldTime;
+    hour    = diff(4);
+    minute  = diff(5);
+    second  = diff(6);
+    oldTime = clock;
+end
+
+function bool = isTooLong(hour,minute,second)
+    bool = false;
+    if second > 30
+        bool = true;
+    elseif minute > 0
+        bool = true;
+    elseif hour > 0
+        bool = true;
+    end
+end
