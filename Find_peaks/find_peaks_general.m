24bc114649dd5b6496e8c4b41c8f38943bb10f45
LorRic-01 <61011479+LorRic-01@users.noreply.github.com>
2020-03-01 22:21:16 +0100

    Find_peaks revolution
    Rivoluzionato il rilevamento dei picchi
    - Più compatto e leggibile (in inglese)
    - Utilizzo del filtro di Kalman (solo in alcuni casi)
    - Utilizzabile per vettori di qualsiasi dimensione e lunghezza

---
+++ Find_peaks/find_peaks_general.m
@@ -0,0 +1,41 @@
+%% Input
+
+% t             - time                                                  [double[]]
+% y             - measures                                              [double[]]
+% degree        - max degree during poly fit evaluation                 [int]
+% num           - number of elements evaluating during polyfit          [int]
+% gap           - maximum permissible percentage error                  [double]
+% varp_error    - percentage change of error compared to prediction     [double[]]
+% var2_error    - avarage squared variation of error                    [double[]]
+%               - composed by noise and imprecision of polyval
+
+%% Output
+
+% anomaly       - presence of anomaly in v_next                 [boolean]
+% v_next        - next value (predicted)                        [double[]]
+% error         - difference between prediction and measure     [double[]]
+% varp_error    - ...
+% var2_error    - ...
+
+%% Function
+function [anomaly, v_next, error, varp_error, var2_error] = find_peaks_general(t, y, degree, num, gap, varp_error, var2_error)
+    
+    % not enought elements for polyfit
+    if length(t)<degree+3
+        anomaly = false;
+        v_next = y(:,end);
+        error = zeros(size(v_next));
+        return
+    end
+    
+    start = max(length(t)-num, 1);
+    % Vector for polyfit creation and next point evaluation
+    t_poly = t(start:end);
+    y_poly = y(:,start:end);
+    
+    % sigma - precision of polyval evaluation [double[]]
+    [v_next, error, sigma, ~] = poly_fit(t_poly, y_poly, degree);
+    
+    [anomaly, varp_error, var2_error] = peak_presence_general(error, v_next, gap, sigma, varp_error, var2_error);
+    
+end
