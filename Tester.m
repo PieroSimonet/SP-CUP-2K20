5e09def0d2872c83d3a71fa9e7f9bc70109f90ee
GabrieleFerra <gabriele.ferraresso@gmail.com>
2020-03-08 21:36:06 +0100

    aggiunta la possibilità di caricare più punti in una chiamata

--- Tester.m
+++ Tester.m
@@ -6,8 +6,8 @@ x.b=randn(50,3);
 for i=1:50
    [last, Anomalia,posizioneA,h, s]=IsolationForest(100,50,0.6,"a",x.a(i,:));
    disp(posizioneA);
-%    [last2, Anomalia2,posizioneA2,h2, s2]=IsolationForest(100,20,0.6,"b",x.b(i,:));
-%    disp(posizioneA2)
+%     [last2, Anomalia2,posizioneA2,h2, s2]=IsolationForest(100,50,0.6,"b",x.b(i,:));
+%     disp(posizioneA2)
 end
 posizioneA=s>0.6;
 plot3(x.a(:,1),x.a(:,2),x.a(:,3),'go','MarkerFaceColor','g','MarkerSize',12)
@@ -15,4 +15,3 @@ hold on
 
 plot3(x.a(posizioneA,1),x.a(posizioneA,2),x.a(posizioneA,3),'ro','MarkerFaceColor','r','MarkerSize',12)
 hold off
-legend('Normal points','Anomalies')
