f6a3f0e11fe4ad3b5ab9b7b930b0218a38f97e01
Piero Simonetto <simonetto.piero@gmail.com>
2020-03-12 09:39:57 +0100

    partial implementation Optimize

--- InizializeNameOfFiles.m
+++ InizializeNameOfFiles.m
@@ -1,6 +1,6 @@
 %file1 = "2020-01-17-11-32-12.bag";
 %file2 = "IMU&camera Drone Synchronized training dataset_normal behabiour_no abnormalities.bag";
-file3 = "IMU_camera_Initial data set for abnormalities training_2 Dec 2019.bag";
+file3 = "IMU&camera_Initial data set for abnormalities training_2 Dec 2019.bag";
 %batteryVoltage = "/mavros/battery";
 %odom = '/mavros/local_position/odom';
 %velocityBody = '/mavros/local_position/velocity_body';
