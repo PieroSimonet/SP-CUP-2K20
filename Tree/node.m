c8fe7fe0ab107fb6c588fd2058b1f9a83c3878e5
darkgap <mrcp98@gmail.com>
2020-03-22 14:34:49 +0100

    added node class

---
+++ Tree/node.m
@@ -0,0 +1,33 @@
+% Container Class representing a node
+
+classdef node
+	properties
+		name
+		time
+		data
+	end
+
+	methods
+		% Constructor
+		function obj = node(m_name,m_time,m_data)
+			if nargin == 1
+				m_time = [];
+				m_data = [];
+			end
+			
+			if(strcmpi(m_name,'clear'))
+				m_name = '';
+			end
+
+			obj.name = m_name;
+			obj.time = m_time;
+			obj.data = m_data;
+		end
+		
+		% Push new Data into the node
+		function obj = pushData(obj, time,data)
+			obj.time = [ obj.time time];
+			obj.time = [ obj.data data];
+		end
+	end
+end
